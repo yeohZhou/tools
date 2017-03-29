dc656bfe48452b01e1059c42bda55cef3d5854b6
Yeoh zhou <Yeohzhou@zhouyangdeMacBook-Pro.local>
2017-03-29 14:35:15 +0800

    1
    1

---
+++ urllib2.py
@@ -0,0 +1,24 @@
+import requests
+import urllib2
+import sys
+import threading
+
+def getTitle(url):
+    try:
+	    _ = re.search('<title>(.*?)</title>', urllib2.urlopen(url,timeout=8).read())  
+	    print url, _.group(1)+'\n'
+    except:  
+        pass
+
+def createIp(ip,port):
+	tmp = []
+	for x in range(255): 
+		tmp.append('http://'+str(ip)+'.'+str(x)+':'+str(port)+'/')
+	return tmp
+
+if __name__ == '__main__':
+	ip,port = sys.argv[1:]
+	urls = createIp(ip,port)
+	for i in urls:
+		th = threading.Thread(target = getTitle, args = (i,))
+		th.start()
